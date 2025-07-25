3870793d957e2ed21d8811290039f33e04de2b01
Sergei Iazev <shass@bk.ru>
2025-07-25 17:31:18 +0300

    feat: update hero banner

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -2,13 +2,13 @@ import Image from "next/image"
 
 export function HeroSection() {
   return (
-    <section className="w-full pt-2 pb-2">
+    <section className="w-full pt-16 pb-2">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="text-center">
           <div className="mb-2">
             <Image
               src="/hero-banner.png"
-              alt="Your users need to be HEARD"
+              alt="Everyone will be HEARD"
               width={800}
               height={400}
               className="mx-auto max-w-full h-auto"
