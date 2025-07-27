3043767f33a2a17b10a472cae64bad849de52eda
Sergei Iazev <shass@bk.ru>
2025-07-27 21:10:13 +0300

    feat: chage font to Bahnschrift

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -19,6 +19,10 @@ const config: Config = {
       },
     },
     extend: {
+      fontFamily: {
+        'bahnschrift': ['Bahnschrift', 'sans-serif'],
+        'sans': ['Bahnschrift', 'system-ui', 'sans-serif'],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
