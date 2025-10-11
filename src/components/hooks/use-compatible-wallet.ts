706416ae2d3aa527cc5ff04ea5a6e83d1e2fcbf6
Sergei Iazev <shass@bk.ru>
2025-10-11 14:36:52 +0300

    feat: wip: HUGE rewrite

--- src/components/hooks/use-compatible-wallet.ts
+++ src/components/hooks/use-compatible-wallet.ts
@@ -1,18 +1,37 @@
 'use client'
 
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
-import { useConnectModal } from '@rainbow-me/rainbowkit'
+import { usePlatformDetector } from '@/src/platforms/_core/PlatformDetectorProvider'
+import { useWebWallet } from '@/src/platforms/web/hooks/useWebWallet'
+import { useBaseAppWallet } from '@/src/platforms/base-app/hooks/useBaseAppWallet'
+import { useFarcasterWallet } from '@/src/platforms/farcaster/hooks/useFarcasterWallet'
+import { Platform } from '@/src/platforms/config'
 
 export function useCompatibleWallet() {
-  const { isConnected, address, isConnecting } = useAccount()
-  const { disconnect } = useDisconnect()
-  const { openConnectModal } = useConnectModal()
+  const { platform } = usePlatformDetector()
+
+  // Call all hooks (React rules)
+  const webWallet = useWebWallet()
+  const baseAppWallet = useBaseAppWallet()
+  const farcasterWallet = useFarcasterWallet()
+
+  // Select the appropriate wallet based on platform
+  const wallet = (() => {
+    switch (platform) {
+      case Platform.BASE_APP:
+        return baseAppWallet
+      case Platform.FARCASTER:
+        return farcasterWallet
+      case Platform.WEB:
+      default:
+        return webWallet
+    }
+  })()
 
   return {
-    isConnected,
-    address,
-    isConnecting,
-    connect: openConnectModal,
-    disconnect,
+    isConnected: wallet?.isConnected || false,
+    address: wallet?.address || null,
+    isConnecting: wallet?.isLoading || false,
+    connect: wallet?.connect || (async () => {}),
+    disconnect: wallet?.disconnect || (async () => {}),
   }
 }
