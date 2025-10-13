dfd9959a89ff5c0577c8c349f2b156b9c9ef8958
Sergei Iazev <shass@bk.ru>
2025-10-13 16:19:42 +0300

    feat: wip: HUGE refactor - wip base app add Query Client Provider

--- src/platforms/base-app/layouts/BaseAppLayout.tsx
+++ src/platforms/base-app/layouts/BaseAppLayout.tsx
@@ -1,6 +1,8 @@
 'use client'
 
 import { ReactNode } from 'react'
+import { QueryClientProvider } from '@tanstack/react-query'
+import { queryClient } from '@/src/platforms/web/providers/Web3Provider'
 import { CreateSurveyModalProvider } from '@/components/providers/create-survey-modal-provider'
 import { NavigationProvider } from '@/components/providers/navigation-provider'
 import { NotificationContainer } from '@/components/ui/notifications'
@@ -11,14 +13,15 @@ interface BaseAppLayoutProps {
 }
 
 export default function BaseAppLayout({ children }: BaseAppLayoutProps) {
-
   return (
-    <CreateSurveyModalProvider>
-      <NavigationProvider>
-        <MiniKitReady />
-        {children}
-        <NotificationContainer />
-      </NavigationProvider>
-    </CreateSurveyModalProvider>
+    <QueryClientProvider client={queryClient}>
+      <CreateSurveyModalProvider>
+        <NavigationProvider>
+          <MiniKitReady />
+          {children}
+          <NotificationContainer />
+        </NavigationProvider>
+      </CreateSurveyModalProvider>
+    </QueryClientProvider>
   )
 }
