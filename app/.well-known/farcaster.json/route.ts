cde86a9f33806dc55ce6013852efa2ebc47003e6
Sergei Iazev <shass@bk.ru>
2025-10-20 16:59:31 +0300

    feat: update ownerAddress

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -23,7 +23,7 @@ export async function GET() {
     }),
 
     baseBuilder: {
-      allowedAddresses: ["0x17dfdB593be92cd172840530Ef975d16561e47dC, 0xb135CC0182C9F009ABe8af67adbf0dc8ed0a0525"]
+      ownerAddress: "0x17dfdB593be92cd172840530Ef975d16561e47dC"
     },
 
     // Core frame config
