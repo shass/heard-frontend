4b1af955926ccea6af4196c801ce1733a4ed6fb0
Sergei Iazev <shass@bk.ru>
2025-11-30 02:59:31 +0300

    refactor: edit poll func improvements

--- components/features/admin/SurveyForm/index.tsx
+++ components/features/admin/SurveyForm/index.tsx
@@ -46,7 +46,7 @@ export function SurveyForm({ survey, onSubmit, isLoading, onCancel }: SurveyForm
         isEditMode={!!survey}
       />
 
-      <RewardsSection register={register} errors={errors} />
+      <RewardsSection register={register} watch={watch} errors={errors} />
       
       <QuestionsSection
         questions={questions}
